5aa539a71366461cdb8290bd7baa71d7c02f077e
Nayane Santos <nayanemenezes2011@gmail.com>
2021-07-03 14:03:22 -0300

    :package: nova funcionalidade

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import { useEffect, useState } from "react";
+import SearchInput from "./components/searchInput";
+import './styles/styles.css'
+
+const api = 'https://kitsu.io/api/edge/'
 
 function App() {
+  const [info, setInfo] = useState({})
+  const [text,setText]= useState('');
+ 
+  useEffect(() => {
+    if (text){
+    fetch(`${api}anime?filter[text]=${text}&page[limit]=10`)
+    .then((response) => response.json())
+    .then((response) => {
+      setInfo(response)
+    }) 
+    }
+  },[text])
+
   return (
-    <h1>Olá povo!</h1>
+    <div className='App'>
+    <h1>Animes!</h1>
+    <SearchInput value={text} onChange={(search) => setText(search)}/>
+    {info.data && (
+      <ul className='animes-list'>
+        {info.data.map((anime) => (
+          <li key={anime.id}>
+            <img src={anime.attributes.posterImage.small} alt={anime.attributes.canonicalTitle}/>
+            {anime.attributes.canonicalTitle}
+          </li>
+        ))}
+      </ul>
+    )}
+    </div>
   );
 }
 
